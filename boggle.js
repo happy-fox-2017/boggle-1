5035134d7bab3fcffe5ed4adb28c3b506008b2c2
rusli abdul gani <rusli.gani88@gmail.com>
2017-06-05 01:00:07 +0700

    boggle-1 done

--- boggle.js
+++ boggle.js
@@ -8,17 +8,16 @@ class Boggle{
         let board=[];
         let random=[];
         let huruf=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-        for(let i=0; i<Math.pow(this._matriks,2);i++)
-            {
+        for(let i=0; i<Math.pow(this._matriks,2);i++){
               let index=Math.floor(Math.random()*huruf.length);
               random.push(huruf[index]);
             }
-        for(let i=0; i<this._matriks;i++)
+        for(let i=0; i<this._matriks;i++){
         board.push(random.slice(i,i+this._matriks));
-
+            }
         return board;
         }
 }
 
-let game = new Boggle(3);
+let game = new Boggle(5);
 console.log(game.acak());
